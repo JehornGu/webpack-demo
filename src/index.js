45c2dee5d60c31421af2cb6b55215cd57677d50d
Jehorn <gujh@bbdtek.com>
2019-04-24 16:29:51 +0800

    webpack笔记三 管理输出

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
+import printMe from './print';
 
 function component() {
     let element = document.createElement('div');
+    let btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    btn.innerHTML = 'Click here, then watch the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
 
     return element;
 }
